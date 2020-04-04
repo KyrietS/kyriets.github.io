4b0d8482b1e67a9ae88bb86a7caf3c3d470c182e
KyrietS <fojcik.sebastian@gmail.com>
2020-04-04 19:57:57 +0000

    Update master to output generated at 97b2df2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 // Side navigation
 function nav_open() {
-    var x = document.getElementById("navigation");
-    x.style.display = "block";
-  }
+  var x = document.getElementById("navigation");
+  x.style.display = "block";
+}
   
-  function nav_close() {
-    document.getElementById("navigation").style.display = "none";
-  }
-
-
-  // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+function nav_close() {
+  document.getElementById("navigation").style.display = "none";
+}
 
 var navbar = document.getElementById("navigation");
 var stickyOffet = navbar.offsetTop;
@@ -38,31 +34,3 @@ window.addEventListener("scroll", function() {
     }
     prevScrollpos = currentScrollPos;
 });
-
-// // Get the navbar
-// var navbar = document.getElementById("navigation");
-// var mobileNavbar = document.getElementById("mobile-navbar");
-
-// Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//       navbar.classList.add("sticky")
-//     } else {
-//       navbar.classList.remove("sticky");
-//     }
-//   }
-
-//   var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     navbar.style.top = "0";
-//     this.navbar.classList.add("sticky");
-//   } else {
-//     navbar.style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
