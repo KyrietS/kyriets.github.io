b4ecb416c51bf88ea11a6b11272c4eb8f7d5de6a
KyrietS <fojcik.sebastian@gmail.com>
2020-04-14 21:31:58 +0000

    Update master to output generated at 98a86c0

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 // Pasek nawigacyjny
-const dropdownCategories = document.getElementById('dropdown-categories');
-const dropdownCategoriesButton = document.getElementById('dropdown-categories-button');
 
 // Zapobieganie schowaniu menu przy próbie wciśniecia w jedną z opcji
-dropdownCategories.addEventListener('mouseenter', () => window.menuCategories=true);
-dropdownCategories.addEventListener('mouseleave', () => window.menuCategories=false);
-
+document.querySelectorAll('.w3-dropdown-content').forEach(function(content) {
+    content.addEventListener('mouseenter', () => window.dropdownActive=true);
+    content.addEventListener('mouseleave', () => window.dropdownActive=false);
+});
+// Podmenu Kategorie
+const dropdownCategories = document.getElementById('dropdown-categories');
+const dropdownCategoriesButton = document.getElementById('dropdown-categories-button');
 dropdownCategoriesButton.addEventListener('click', () => dropdownCategories.classList.toggle('w3-show'));
 dropdownCategoriesButton.addEventListener('blur', () => {
-    if (!window.menuCategories) {
+    if (!window.dropdownActive) {
         dropdownCategories.classList.remove('w3-show');
     }
 });	
+// Szukajka
+const dropdownSearch = document.getElementById('dropdown-search');
+const dropdownSearchInput = document.getElementById('dropdown-search-input');
+const dropdownSearchInfo = document.getElementById('dropdown-search-info');
+dropdownSearchInput.addEventListener('focus', () => dropdownSearch.classList.add('w3-show'));
+dropdownSearchInput.addEventListener('blur', () => {
+    if (!window.dropdownActive) {
+        dropdownSearch.classList.remove('w3-show');
+    }
+});	
+dropdownSearchInfo.addEventListener('mousedown', () => dropdownSearch.classList.remove('w3-show'));
 
-// Side navigation
+// Menu boczne na telefonie
 function nav_open() {
-    var x = document.getElementById("navigation");
-    x.style.display = "block";
+    const nav = document.getElementById("navigation");
+    nav.style.display = "block";
 }
   
 function nav_close() {
-    document.getElementById("navigation").style.display = "none";
+    const nav = document.getElementById("navigation");
+    nav.style.display = "none";
 }
 
+// Automatyczne przyklejanie się paska menu
 var navbar = document.getElementById("navigation");
 var stickyOffet = navbar.offsetTop;
 window.addEventListener("scroll", function() {
@@ -33,7 +48,7 @@ window.addEventListener("scroll", function() {
     }
 });
 
-
+// Automatyczne chowanie się paska menu
 var prevScrollpos = window.pageYOffset;
 window.addEventListener("scroll", function() {
     var mobileNavbar = document.getElementById("mobile-navbar");
